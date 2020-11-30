aba1e6424d3e6560688c75f3b60ae5184e8902ff
Balint Soos <soosbalint95@gmail.com>
2020-11-30 21:53:49 +0100

    better solution for year 2019 day 03 part 1

--- src/years/2019/days/03/parts/1/index.ts
+++ src/years/2019/days/03/parts/1/index.ts
@@ -14,7 +14,7 @@ export const readLines = (filePath: string): string[][] => {
   return [lines[0], lines[1]];
 };
 
-export const newCoord = (
+export const getNextCoord = (
   currentCoord: Coordinate,
   direction: string
 ): Coordinate => {
@@ -36,44 +36,39 @@ export const newCoord = (
   }
 };
 
-export const buildCoords = (
-  instructions: string[],
-  origo: Coordinate
-): Coordinate[] => {
-  const coords: Coordinate[] = [];
-  instructions.forEach((instruction: string) => {
-    const direction = instruction[0];
-    const stepCount = parseInt(instruction.slice(1));
-    for (let step = 0; step < stepCount; step++) {
-      const currentCord = coords[coords.length - 1] ?? origo;
-      coords.push(newCoord(currentCord, direction));
-    }
-  });
-  return coords;
-};
-
 export const distance = (a: Coordinate, b: Coordinate): number => {
   return Math.abs(a.posX - b.posX) + Math.abs(a.posY - b.posY);
 };
 
-export const intersection = (
-  listA: Coordinate[],
-  listB: Coordinate[]
-): Coordinate[] =>
-  listA.filter(
-    (coordA) =>
-      !!listB.find(
-        (coordB) => coordA.posX === coordB.posX && coordA.posY === coordB.posY
-      )
-  );
-
 export const processLines = (lines: string[][]): number => {
-  return intersection(
-    buildCoords(lines[0], ORIGO),
-    buildCoords(lines[1], ORIGO)
-  )
-    .map((intersection) => distance(ORIGO, intersection))
-    .reduce((prev, curr) => Math.min(prev, curr));
+  const distances: number[] = [];
+  const coords = new Map<string, number>();
+
+  let currentCoord = ORIGO;
+  lines[0].forEach((instruction: string) => {
+    const direction = instruction[0];
+    const stepCount = parseInt(instruction.slice(1));
+    for (let step = 0; step < stepCount; step++) {
+      const newCoord = getNextCoord(currentCoord, direction);
+      coords.set(`${newCoord.posX}-${newCoord.posY}`, 1);
+      currentCoord = newCoord;
+    }
+  });
+
+  currentCoord = ORIGO;
+  lines[1].forEach((instruction: string) => {
+    const direction = instruction[0];
+    const stepCount = parseInt(instruction.slice(1));
+    for (let step = 0; step < stepCount; step++) {
+      const newCoord = getNextCoord(currentCoord, direction);
+      if (coords.has(`${newCoord.posX}-${newCoord.posY}`)) {
+        distances.push(distance(ORIGO, newCoord));
+      }
+      currentCoord = newCoord;
+    }
+  });
+
+  return Math.min(...distances);
 };
 
 export default async (): Promise<number> => {
