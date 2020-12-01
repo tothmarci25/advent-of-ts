bc8f3a88a33b6668ae570e650b4dfd05ae3718ba
Balint Soos <soosbalint95@gmail.com>
2020-12-01 12:23:09 +0100

    solve year 2020 day 01 part 1

--- src/years/2020/days/01/parts/1/index.spec.ts
+++ src/years/2020/days/01/parts/1/index.spec.ts
@@ -1,7 +1,16 @@
-import solve from ".";
+import solve, { processInput } from ".";
 
 describe("Year 2020 - Day 01 - Part 1", () => {
-  it("should return solution", async () => {
-    expect(await solve()).toEqual(1);
+  describe("#processInput", () => {
+    it("should return solution", () => {
+      const input = [1721, 979, 366, 299, 675, 1456];
+      expect(processInput(input)).toEqual(514579);
+    });
+  });
+
+  describe("#default", () => {
+    it("should return solution", async () => {
+      expect(await solve()).toEqual(605364);
+    });
   });
 });
--- src/years/2020/days/01/parts/1/index.ts
+++ src/years/2020/days/01/parts/1/index.ts
@@ -1,3 +1,24 @@
-export default async (): Promise<number> => {
-  return 1;
+import { readFileSync } from "fs";
+import { join } from "path";
+
+export const readInput = (filePath: string): number[] => {
+  const file = readFileSync(filePath, { encoding: "utf8" });
+  const lines = file.split("\n").map((line) => parseInt(line));
+  lines.pop();
+  return lines;
+};
+
+export const processInput = (input: number[]): number | void => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      if (input[i] + input[j] === 2020) {
+        return input[i] * input[j];
+      }
+    }
+  }
+};
+
+export default async (): Promise<number | void> => {
+  const input = readInput(join(__dirname, "../../input.txt"));
+  return processInput(input);
 };
