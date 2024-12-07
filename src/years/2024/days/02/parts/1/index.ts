40a2c815d5bb3563f553d5ca33bc3fc1c724756c
Balint Soos <soosbalint95@gmail.com>
2024-12-07 22:45:23 +0100

    solve 2024 day 2 part 1

---
+++ src/years/2024/days/02/parts/1/index.ts
@@ -0,0 +1,48 @@
+import { join } from "path";
+import { readLines } from "../../../../../../lib/readLines";
+
+export default async (): Promise<number> => {
+  const inputPath = join(__dirname, "../../input.txt");
+  let safeLevelsCount = 0;
+  await readLines(inputPath, (line) => {
+    const levels = line.split(" ");
+    if (isSafeReport(levels)) {
+      safeLevelsCount += 1;
+    }
+  });
+  return safeLevelsCount;
+};
+
+function isSafeReport(levels: string[]): boolean {
+  const diffs = getLevelDiffs(levels);
+  return (
+    isAtLeastOne(diffs) &&
+    isAllAtMostThree(diffs) &&
+    (isAllDecreasing(diffs) || isAllIncreasing(diffs))
+  );
+}
+
+function getLevelDiffs(levels: string[]): number[] {
+  const diffs: number[] = [];
+  levels.reduce((prev, next) => {
+    diffs.push(parseInt(prev) - parseInt(next));
+    return next;
+  });
+  return diffs;
+}
+
+function isAtLeastOne(diffs: number[]): boolean {
+  return diffs.every((diff) => Math.abs(diff) >= 1);
+}
+
+function isAllAtMostThree(diffs: number[]): boolean {
+  return diffs.every((diff) => Math.abs(diff) <= 3);
+}
+
+function isAllDecreasing(diffs: number[]): boolean {
+  return diffs.every((diff) => diff > 0);
+}
+
+function isAllIncreasing(diffs: number[]): boolean {
+  return diffs.every((diff) => diff < 0);
+}
