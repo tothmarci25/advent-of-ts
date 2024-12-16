aedb8bcd81050e5b6c47b3e0a04708c66db7a9fa
Balint Soos <soosbalint95@gmail.com>
2024-12-16 16:42:57 +0100

    refactor 2024 day 3 part 1

--- src/years/2024/days/03/parts/1/index.ts
+++ src/years/2024/days/03/parts/1/index.ts
@@ -7,18 +7,15 @@ export default async (): Promise<number> => {
   let result = 0;
   const lines = readLines(inputPath);
   for await (const line of lines) {
-    result += parseMul(line);
+    for (const mul of parseMul(line)) {
+      const [, x, y] = mul;
+      result += parseInt(x) * parseInt(y);
+    }
   }
   return result;
 };
 
-function parseMul(line: string): number {
-  const regex = /mul\((\d+),(\d+)\)/g;
-  let match;
-  let result = 0;
-  while ((match = regex.exec(line)) !== null) {
-    const [, x, y] = match;
-    result += parseInt(x) * parseInt(y);
-  }
-  return result;
+function parseMul(line: string) {
+  const mulRegex = /mul\((\d+),(\d+)\)/g;
+  return line.matchAll(mulRegex);
 }
