743d9c7a7cf842e97fd706fd1e53c44463930a34
Balint Soos <soosbalint95@gmail.com>
2024-12-22 20:52:28 +0100

    solve 2024 day 6 part 1

--- src/years/2024/days/06/parts/1/index.spec.ts
+++ src/years/2024/days/06/parts/1/index.spec.ts
@@ -1,31 +1,65 @@
-import solve, { getNextDirection } from ".";
+import solve, { DIRECTION, getNextPosition, getNextDirection } from ".";
 
 describe("Year 2024 - Day 06 - Part 1", () => {
+  describe("#getNextPosition", () => {
+    it("should return next position when given up", () => {
+      const result = getNextPosition({
+        position: [10, 10],
+        direction: DIRECTION.UP,
+      });
+      expect(result).toEqual([10, 9]);
+    });
+
+    it("should return next position when given right", () => {
+      const result = getNextPosition({
+        position: [10, 10],
+        direction: DIRECTION.RIGHT,
+      });
+      expect(result).toEqual([11, 10]);
+    });
+
+    it("should return next position when given down", () => {
+      const result = getNextPosition({
+        position: [10, 10],
+        direction: DIRECTION.DOWN,
+      });
+      expect(result).toEqual([10, 11]);
+    });
+
+    it("should return next position when given left", () => {
+      const result = getNextPosition({
+        position: [10, 10],
+        direction: DIRECTION.LEFT,
+      });
+      expect(result).toEqual([9, 10]);
+    });
+  });
+
   describe("#getNextDirection", () => {
-    it("should calculate result", () => {
-      const result = getNextDirection();
-      expect(result).toEqual(testCase.expected);
+    it("should return right direction when given up", () => {
+      const result = getNextDirection(DIRECTION.UP);
+      expect(result).toEqual(DIRECTION.RIGHT);
     });
 
-    it("should calculate result", () => {
-      const result = getNextDirection();
-      expect(result).toEqual(testCase.expected);
+    it("should return down direction when given right", () => {
+      const result = getNextDirection(DIRECTION.RIGHT);
+      expect(result).toEqual(DIRECTION.DOWN);
     });
 
-    it("should calculate result", () => {
-      const result = getNextDirection();
-      expect(result).toEqual(testCase.expected);
+    it("should return left direction when given down", () => {
+      const result = getNextDirection(DIRECTION.DOWN);
+      expect(result).toEqual(DIRECTION.LEFT);
     });
 
-    it("should calculate result", () => {
-      const result = getNextDirection();
-      expect(result).toEqual(testCase.expected);
+    it("should return up direction when given left", () => {
+      const result = getNextDirection(DIRECTION.LEFT);
+      expect(result).toEqual(DIRECTION.UP);
     });
   });
 
   describe("#default", () => {
     it("should return solution", async () => {
-      expect(await solve()).toEqual(4814);
+      expect(await solve()).toEqual(4789);
     });
   });
 });
--- src/years/2024/days/06/parts/1/index.ts
+++ src/years/2024/days/06/parts/1/index.ts
@@ -2,10 +2,17 @@ import { join } from "path";
 import { readLines } from "../../../../../../lib/readLines-promise";
 
 type Position = [number, number];
-
+type Direction = [0, -1] | [1, 0] | [0, 1] | [-1, 0];
 type Guard = {
   position: Position;
-  direction: Position;
+  direction: Direction;
+};
+
+export const DIRECTION: { [key: string]: Direction } = {
+  UP: [0, -1],
+  RIGHT: [1, 0],
+  DOWN: [0, 1],
+  LEFT: [-1, 0],
 };
 
 const inputPath = join(__dirname, "../../input.txt");
@@ -13,48 +20,59 @@ const inputPath = join(__dirname, "../../input.txt");
 export default async (): Promise<number> => {
   const guard: Guard = {
     position: [-1, -1],
-    direction: [0, -1],
+    direction: DIRECTION.UP,
   };
-
-  const touchedPositions = new Set<string>();
-
+  const visitedPositions = new Set<string>();
   const lines: string[] = [];
   for await (const line of readLines(inputPath)) {
-    const guardYPosition = line.indexOf("^");
-    if (guardYPosition !== -1) {
-      guard.position = [lines.length, guardYPosition];
-      touchedPositions.add(guard.position.join(","));
+    const guardXPosition = line.indexOf("^");
+    if (guardXPosition !== -1) {
+      guard.position = [guardXPosition, lines.length];
+      visitedPositions.add(guard.position.join(","));
     }
     lines.push(line);
   }
+  return processLines(lines, guard, visitedPositions);
+};
+
+export function processLines(
+  lines: string[],
+  guard: Guard,
+  visitedPositions: Set<string>
+): number {
+  // eslint-disable-next-line no-constant-condition
   while (true) {
-    const [x, y] = guard.position;
-    const [dx, dy] = guard.direction;
-    const nextPosition: Position = [x + dx, y + dy];
+    const nextPosition = getNextPosition(guard);
     const [nx, ny] = nextPosition;
     if (nx < 0 || nx >= lines[0].length || ny < 0 || ny >= lines.length) {
       break;
     }
-    const nextChar = lines[nx][ny];
+    const nextChar = lines[ny][nx];
     if (nextChar === "#") {
-      const nextDirection = getNextDirection(guard.position);
-      if (nextDirection) {
-        guard.direction = nextDirection;
-      }
+      guard.direction = getNextDirection(guard.direction);
     } else {
       guard.position = nextPosition;
-      touchedPositions.add(nextPosition.join(","));
+      visitedPositions.add(nextPosition.join(","));
     }
   }
-  return touchedPositions.size;
-};
+  return visitedPositions.size;
+}
 
-export function getNextDirection([x, y]: Position): Position | undefined {
-  const directions: Position[] = [
-    [0, -1], // up
-    [1, 0], // right
-    [0, 1], // down
-    [-1, 0], // left
-  ];
-  return directions.find(([dx, dy]) => dx !== -x && dy !== -y);
+export function getNextPosition(guard: Guard): Position {
+  const [x, y] = guard.position;
+  const [dx, dy] = guard.direction;
+  return [x + dx, y + dy];
+}
+
+export function getNextDirection(direction: Direction): Direction {
+  if (direction === DIRECTION.UP) {
+    return DIRECTION.RIGHT;
+  }
+  if (direction === DIRECTION.RIGHT) {
+    return DIRECTION.DOWN;
+  }
+  if (direction === DIRECTION.DOWN) {
+    return DIRECTION.LEFT;
+  }
+  return DIRECTION.UP;
 }
